ca4b7fa5d3cb3b4c5c7a586fd4fc8b4837fc3ecc
LivStokes <Oliviastokess02@gmail.com>
2025-08-27 05:37:11 +0100

    Add files via upload

---
+++ Loan_Calculator/index.js
@@ -0,0 +1,9 @@
+function calculateLoan() {
+    loanAmountValue = document.getElementById("loan-amount").value;
+
+    interestRateValue = document.getElementById("interest-rate").value;
+
+    MonthsToPayValue = document.getElementById("months-to-pay").value;
+
+    
+}
